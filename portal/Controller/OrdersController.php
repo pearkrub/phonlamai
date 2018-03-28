fc4ca292b5192ec229acfe1ec7b2b2d2604345e1
Praibool Namwong <praibool.piak@gmail.com>
2018-03-28 09:35:48 +0700

    order_list

--- portal/Controller/OrdersController.php
+++ portal/Controller/OrdersController.php
@@ -2,12 +2,27 @@
 
 class OrdersController extends AppController
 {
+    public $uses = array('Order', 'OrderDetail');
     public function index() {
         $auth = $this->Session->read('Auth');
         if(empty($auth)){
             $this->redirect('/login');
         }
+
+        $orderIds = $this->OrderDetail->find('list',array(
+            'conditions' => array(
+                'OrderDetail.merchant_id' => $auth['Merchant']['id']
+            ),
+            'fields' => array(
+                'OrderDetail.order_id',
+                'OrderDetail.order_id'
+            )
+        ));
         $orders = $this->Order->find('all', array(
+            'conditions' => array(
+                'Order.step >' => 1,
+                'Order.id' => $orderIds
+            ),
             'order' => array('Order.id desc')
         ));
         $this->set('orders', $orders);
